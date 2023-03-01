7c6350a3916b84bc355f172f369494ce66159cea
YonasValentin <83760534+YonasValentin@users.noreply.github.com>
2023-03-01 17:47:00 +0100

    edit: time changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ const App = () => {
     const portOfLoading = 'Karlshamn';
     const portOfDischarge = 'Klaipeda';
     //Year, Month (starts with 0), Day, Hour, Minute, Second, MileSecond
-    const departureTime = new Date(2023, 2, 1, 8, 0, 2, 0);
-    const arrivalTime = new Date(2023, 2, 1, 21, 0, 2, 0);
+    const departureTime = new Date(2023, 2, 1, 8, 0, 0, 0);
+    const arrivalTime = new Date(2023, 2, 1, 13, 0, 0, 0);
 
     return (
         <>
